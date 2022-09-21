838b6fca8171d8091508826ac9b1c12fff7669f4
Vitali Rapalis <vitali.rapalis@googlemail.com>
2022-09-21 17:43:36 +0200

    feat(demo-component): implemented demo component in stencil

--- apps/angular-client/src/app/app.component.ts
+++ apps/angular-client/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <my-component first="Angular" last="Application!!!"></my-component>
+<!--    <my-component first="Angular" last="Application!!!"></my-component>-->
   `,
   styles: []
 })
--- apps/angular-client/src/app/app.module.ts
+++ apps/angular-client/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {ComponentLibraryModule} from "vrapalis-angular-components";
+// import {ComponentLibraryModule} from "vrapalis-angular-components";
 
 @NgModule({
   declarations: [
@@ -10,7 +10,7 @@ import {ComponentLibraryModule} from "vrapalis-angular-components";
   ],
   imports: [
     BrowserModule,
-    ComponentLibraryModule
+    // ComponentLibraryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
