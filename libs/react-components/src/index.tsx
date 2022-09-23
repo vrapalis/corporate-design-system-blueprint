d76e1eb9384a4e795ddfb330f16d2a3a45c65e23
Vitali Rapalis <vitali.rapalis@googlemail.com>
2022-09-23 16:50:58 +0200

    fix(react): fix react tests
    fix of react library tests

--- libs/react-components/src/index.tsx
+++ libs/react-components/src/index.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react'
 import styles from './styles.module.css'
 
-export * from './components/generated';
+export * from './components/generated'
 
 interface Props {
   text: string
 }
 
-export const ExampleComponent = ({text}: Props) => {
+export const ExampleComponent = ({ text }: Props) => {
   return <div className={styles.test}>Example Component: {text}</div>
 }
